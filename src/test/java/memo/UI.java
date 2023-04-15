606ce4a12decd3f112c1d5adcbf9aadbfc7d4efe
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-04-15 12:37:48 +0300

    pom update added memo

---
+++ src/test/java/memo/UI.java
@@ -0,0 +1,41 @@
+package memo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class UI {
+    WebDriver wd;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+        wd.get(Datas.urlWeb);
+        wd.findElement(By.cssSelector("input#inputEmail")).sendKeys(Datas.librarin);
+        wd.findElement(By.cssSelector("input#inputPassword")).sendKeys(Datas.pass);
+        wd.findElement(By.tagName("button")).click();
+    }
+
+    String email;
+
+    @DisplayName("creating user")
+    @Test
+    void create_user() throws InterruptedException {
+        email="abc1@abc.com";
+        Thread.sleep(3000);
+        wd.findElement(By.xpath("(//ul/li//a)[2]")).click();
+        Thread.sleep(1500);
+        wd.findElement(By.cssSelector("a.btn.btn-lg.btn-outline.btn-primary.btn-sm")).click();
+        Thread.sleep(1500);
+        wd.findElement(By.xpath("//input[@name='password']")).sendKeys("pass123");
+        wd.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+        wd.findElement(By.xpath("//input[@name='full_name']")).sendKeys("hattori hanzo");
+    }
+
+}
