6f3597ef2d0a66ee67b980d325ba5239cd9719ce
yakup öztürk <yakupozturk29@hotmail.com>
2023-05-22 07:55:20 +0300

    Api utils added

--- src/test/java/com/library/stepdefinitions/Edit_Book_StepDefs.java
+++ src/test/java/com/library/stepdefinitions/Edit_Book_StepDefs.java
@@ -1,16 +1,22 @@
 package com.library.stepdefinitions;
 
 import com.library.pages.BookEditPage;
+import com.library.utilities.ApiUtil;
 import com.library.utilities.Driver;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 
 public class Edit_Book_StepDefs {
     BookEditPage bookEditPage=new BookEditPage();
+    Response response;
     @When("user click books")
     public void user_click_books() throws InterruptedException {
         bookEditPage.books.click();
@@ -41,5 +47,35 @@ public class Edit_Book_StepDefs {
         Assertions.assertTrue(bookEditPage.savedBookWrite.isDisplayed());
 
 
+    }
+
+    @Given("I logged Bookit api using {string} and {string}")
+    public void iLoggedBookitApiUsingAnd(String email, String password) {
+
+
+    }
+
+    @When("I send POST request to {string} endpoint with {string}{string}{string}{string}{string}{string}")
+    public void iSendPOSTRequestToEndpointWith(String endPoint, String name, String isbn, String year, String author, String book_category, String description) {
+      response = given().accept(ContentType.JSON)
+                .header("x-library-token", ApiUtil.generateToken())
+                .formParam("name", name)
+                .formParam("isbn", isbn)
+                .formParam("year", year)
+                .formParam("author", author)
+                .formParam("book_category_id", 3)
+                .formParam("description", description)
+                .when().post("/add_book");
+
+    }
+
+    @Then("status code should be {int}")
+    public void statusCodeShouldBe(int arg0) {
+Assertions.assertEquals(response.statusCode(),arg0);
+    }
+
+    @And("I shoul see {string} message in response body")
+    public void iShoulSeeMessageInResponseBody(String arg0) {
+        Assertions.assertEquals(response.path("message"),arg0);
     }
 }
--- src/test/resources/features/edit_Book.feature
+++ src/test/resources/features/edit_Book.feature
@@ -10,7 +10,9 @@ Feature: As a librarian I should be able to edit books
     And  user click editBook button
     And user click saveChangesdButton
     Then user can see sucsecfuly saved massage
+
     Scenario:Verify that Librarian can add new books
-      When send a get request "/add_book"
-      And
+      When I send POST request to "/add_book" endpoint with "name""isbn""year""author""book_category_id""description"
+      Then status code should be 200
+      And I shoul see "The book has been created." message in response body
 
